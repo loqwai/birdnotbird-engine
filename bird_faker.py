d41bae05f0b4615be707f9656b111592e7377413
Roy van de Water <roy@circleci.com>
2021-09-18 10:23:22 -0700

    Guaranteed to generate fake birds

--- bird_faker.py
+++ bird_faker.py
@@ -7,16 +7,20 @@ class BirdFaker:
     def __init__(self) -> None:
         self._descriptors = []
         self._varieties = []
+        self._birds = set()
 
     def train(self, filename) -> None:
+        with open(filename) as f:
+            self._birds.update([bird.strip() for bird in f])
+
         all_descriptors = set()
         all_varieties = set()
 
-        with open(filename) as f:
-            for bird in f:
-                *descriptors, variety = bird.strip().split(" ")
-                all_descriptors.update(descriptors)
-                all_varieties.add(variety)
+        for bird in self._birds:
+            *descriptors, variety = bird.strip().split(" ")
+
+            all_descriptors.update(descriptors)
+            all_varieties.add(variety)
 
         self._descriptors = list(set(self._descriptors) | all_descriptors)
         self._varieties = list(set(self._varieties) | all_varieties)
@@ -26,7 +30,12 @@ class BirdFaker:
         descriptors = random.choices(self._descriptors, k=num_descriptors)
         variety = random.choice(self._varieties)
 
-        return " ".join([*descriptors, variety])
+        bird = " ".join([*descriptors, variety])
+
+        if bird in self._birds:
+            return self.fake_bird()
+
+        return bird
 
     def fake_birds(self) -> Iterator[str]:
         yield self.fake_bird()
